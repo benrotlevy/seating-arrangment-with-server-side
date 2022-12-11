60e17dc8ee34237579679e93f2511887bdfb3a14
benrotlevy <benjamin@rotlevy.com>
2022-12-11 15:47:36 +0200

    test

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -5,59 +5,69 @@ import { useAuthContext } from "../context/Context";
 import "./signup.css";
 
 export const Signup = () => {
-  const { token, setToken, setCurrentUser, currentUser } = useAuthContext();
-  //   const [success, setSuccess] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const { token, setToken, setCurrentUser, currentUser } = useAuthContext();
+    //   const [success, setSuccess] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const sendDetailes = async () => {
-    try {
-      const { data } = await usersAPI.post("/users", { name, email, password });
-      setToken(data.token);
-      setCurrentUser(data.user);
-      //   setSuccess(true);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const sendDetailes = async () => {
+        if (!name || !email || !password) return;
+        try {
+            const { data } = await usersAPI.post("/users", {
+                name,
+                email,
+                password,
+            });
+            setToken(data.token);
+            setCurrentUser(data.user);
+            //   setSuccess(true);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  if (token)
+    if (token)
+        return (
+            <div className="login">
+                <h4>you are logged in</h4>
+            </div>
+        );
+
     return (
-      <div className="login">
-        <h4>you are logged in</h4>
-      </div>
+        <div className="signup">
+            <div className="form">
+                <input
+                    name="name"
+                    value={name}
+                    placeholder="name"
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    placeholder="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="password"
+                    minLength="6"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input type="submit" onClick={sendDetailes} value={"signup"} />
+                <p>
+                    if you already have account please{" "}
+                    <Link to="./login">
+                        <span style={{ color: "blue" }}>Login</span>
+                    </Link>
+                </p>
+            </div>
+        </div>
     );
-
-  return (
-    <div className="signup">
-      <div className="form">
-        <input
-          name="name"
-          value={name}
-          placeholder="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          name="email"
-          type="email"
-          value={email}
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={sendDetailes}>signup</button>
-        <p>
-          if you already have account please <Link to="./login">login</Link>
-        </p>
-      </div>
-    </div>
-  );
 };
--- db/mongoose.js
+++ db/mongoose.js
@@ -6,10 +6,10 @@ dotenv.config();
 const uri = process.env.MONGOOSE_URI;
 
 mongoose.connect(uri, (error, mongoConnectionInstance) => {
-  if (error) throw new Error("Mongoose Connection failed, Error: " + error);
-  if (!process.env.NODE_ENV) {
-    const { host, port, name } = mongoConnectionInstance;
-    // console.log(mongoConnectionInstance);
-    console.log({ host, port, name });
-  }
+    if (error) throw new Error("Mongoose Connection failed, Error: " + error);
+    if (!process.env.NODE_ENV) {
+        const { host, port, name } = mongoConnectionInstance;
+        // console.log(mongoConnectionInstance);
+        console.log({ host, port, name });
+    }
 });
